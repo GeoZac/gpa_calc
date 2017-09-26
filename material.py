6b9b0eac1f41b44513883e3cffbbe7d933fea18c
George Zacharia <george.zcharia@gmail.com>
2017-09-26 23:35:30 +0530

    Update MachineHead API
    Slimming down and cleanup

--- material.py
+++ material.py
@@ -14,20 +14,25 @@ def material(mat):
     data = {}
     data['info'] = "an unConventional project"
 
-    #TODO differnetiate material type(isotropc,orthotropic,fiber....)
+    # TODO differnetiate material type(isotropc,orthotropic,fiber....)
     data['data'] = {}
-    #data['data']['material'] = mat.name
+    data['data']['material_name'] = mat.name
     data['data']['type'] = mat.type
+    if mat.type == 'alloy':
+        data['data']['composition'] = {}
+        # TODO add alloy composition,fix add a string field
     data['data']['density'] = mat.density
     data['data']['tensile_strength'] = mat.F1t
     data['data']['shear_strength'] = exp
     data['data']['shear_modulus'] = mat.G12
     data['data']['poisson_ratio'] = mat.nu12
     data['data']['youngs_modulus'] = mat.E1
-    data['source'] = "#reference"
-
+    data['data']['melting_point'] = mat.mp
+    #data['data']['boiling_point'] = mat.bp
+    data['source'] = mat.url
     return data
+
 
 if __name__ == '__main__':
     print(material('steel'))
-#TODO Handle  NaN
+    # TODO Handle 'NaN',known fix = Fill all values
