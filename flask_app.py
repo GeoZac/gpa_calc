69344b70e6d93cde782a5a604434170a30532c59
George <george.zcharia@gmail.com>
2018-04-04 08:38:28 +0530

    suspend project

--- flask_app.py
+++ flask_app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return render_template('index.html')
+    return render_template('inactive.html')
 
 
 @app.route('/main', methods=['GET', 'POST'])
